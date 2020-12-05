f67548e5992967ae0f27233b05b63da03ba104d2
DuGlaser <duglaser0618@gmail.com>
2020-12-05 21:19:14 +0900

    :recycle: Fix the setup_zsh.sh

--- .bin/setup_zsh.sh
+++ .bin/setup_zsh.sh
@@ -2,14 +2,14 @@ set -u
 
 echo "Set up zsh!"
 
-dotfiles="$HOME/dotfiles"
+zsh_setting="$HOME/dotfiles/.zsh_setting"
 
-ln -sfnv "$dotfiles/.p10k.zsh" "$HOME/.p10k.zsh"
-ln -sfnv "$dotfiles/.tmux.conf" "$HOME/.tmux.conf"
-ln -sfnv "$dotfiles/.zshenv" "$HOME/.zshenv"
-ln -sfnv "$dotfiles/.zshrc" "$HOME/.zshrc"
+ln -sfnv "$zsh_setting/.p10k.zsh" "$HOME/.p10k.zsh"
+ln -sfnv "$zsh_setting/.tmux.conf" "$HOME/.tmux.conf"
+ln -sfnv "$zsh_setting/.zshenv" "$HOME/.zshenv"
+ln -sfnv "$zsh_setting/.zshrc" "$HOME/.zshrc"
 
 rm -rf "$HOME/.zsh_custom"
-ln -sfnv "$dotfiles/.zsh_custom" "$HOME/.zsh_custom"
+ln -sfnv "$zsh_setting/.zsh_custom" "$HOME/.zsh_custom"
 
 echo "finish!"

bc1fd42266135a08a9b12b40ce880f7ed7923e25
DuGlaser <duglaser0618@gmail.com>
2021-01-08 10:38:26 +0900

    :sparkles: Add the setup_alacritty script

---
+++ .bin/setup_alacritty.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+sudo apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+cargo build --release
+
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database

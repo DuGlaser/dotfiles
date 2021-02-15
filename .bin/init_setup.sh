5e24b359265f2b1ba7b5aa56f939f0085eef481d
DuGlaser <duglaser0618@gmail.com>
2021-02-15 14:51:06 +0900

    :wrench: Add the asdf setting

--- .bin/init_setup.sh
+++ .bin/init_setup.sh
@@ -8,6 +8,7 @@ ln -sfnv "$dotfiles/.gitconfig" "$HOME/.gitconfig"
 ln -sfnv "$dotfiles/.gitignore_global" "$HOME/.gitignore_global"
 ln -sfnv "$dotfiles/.gitmessage.txt" "$HOME/.gitmessage.txt"
 ln -sfnv "$dotfiles/.tmux.conf" "$HOME/.tmux.conf"
+ln -sfnv "$dotfiles/.asdfrc" "$HOME/.asdfrc"
 
 rm -rf "$HOME/.peco"
 ln -sfnv "$dotfiles/.peco" "$HOME/.peco"

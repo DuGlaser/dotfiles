5adcba0f20bf11edca8dfc5c4cd0166c42bdeaa6
DuGlaser <duglaser0618@gmail.com>
2021-02-16 23:09:03 +0900

    :wrench: Update the clap setting

--- .config/nvim/lua/plugins/clap/setting.lua
+++ .config/nvim/lua/plugins/clap/setting.lua
@@ -1,5 +1,6 @@
 vim.g.clap_enable_icon = 1
 vim.g.insert_mode_only = true
+vim.g.clap_open_preview = 'never'
 
 -- default setting
 vim.cmd("let g:clap_open_action = { 'ctrl-t': 'tab split', 'ctrl-x': 'split', 'ctrl-v': 'vsplit' }")

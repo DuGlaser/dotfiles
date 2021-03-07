d69c231ab347e186778ee39fe2daa5fe65cef9b7
DuGlaser <duglaser0618@gmail.com>
2021-03-07 23:22:35 +0900

    :wrench: Fix the clap preview setting

--- .config/nvim/lua/plugins/clap/setting.lua
+++ .config/nvim/lua/plugins/clap/setting.lua
@@ -1,6 +1,7 @@
 vim.g.clap_enable_icon = 1
 vim.g.insert_mode_only = true
-vim.g.clap_open_preview = 'never'
+vim.g.clap_preview_direction = 'UD'
+-- vim.g.clap_open_preview = 'never'
 
 -- default setting
 vim.cmd("let g:clap_open_action = { 'ctrl-t': 'tab split', 'ctrl-x': 'split', 'ctrl-v': 'vsplit' }")

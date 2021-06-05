1cfcbace90d4a634cf8d3e891cd9f942c1dd5ae0
DuGlaser <duglaser0618@gmail.com>
2021-06-05 14:24:19 +0900

    :wrench: Add the init.sh.

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+chmod +x $HOME/dotfiles/.bin/init_setup.sh
+bash $HOME/dotfiles/.bin/init_setup.sh
+chmod +x $HOME/dotfiles/.config/setup_config.sh
+bash $HOME/dotfiles/.config/setup_config.sh

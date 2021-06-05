7899ff6db1d4cc832d17377cba46517a8ba0b9e4
DuGlaser <duglaser0618@gmail.com>
2021-06-05 14:36:45 +0900

    :wrench: Fix the process to create symbolic links.

--- .bin/init_setup.sh
+++ .bin/init_setup.sh
@@ -1,17 +1,28 @@
+#!/bin/bash
+
 set -u
 
 echo "link home directory dotfiles"
 
 dotfiles="$HOME/dotfiles"
 
-ln -sfnv "$dotfiles/.gitconfig" "$HOME/.gitconfig"
-ln -sfnv "$dotfiles/.gitignore_global" "$HOME/.gitignore_global"
+ln -sfnv "$dotfiles/.gitconfig" "$HOME/.config/git/config"
+ln -sfnv "$dotfiles/.gitignore_global" "$HOME/.config/git/ignore"
 ln -sfnv "$dotfiles/.gitmessage.txt" "$HOME/.gitmessage.txt"
 ln -sfnv "$dotfiles/.tmux.conf" "$HOME/.tmux.conf"
 ln -sfnv "$dotfiles/.asdfrc" "$HOME/.asdfrc"
 
-rm -rf "$HOME/.peco"
+rm "$HOME/.peco"
 ln -sfnv "$dotfiles/.peco" "$HOME/.peco"
+
+dotfiles_config="$dotfiles/.config"
+config="$HOME/.config"
+
+ln -sfnv "$dotfiles_config/alacritty" "$config/alacritty"
+ln -sfnv "$dotfiles_config/bat" "$config/bat"
+ln -sfnv "$dotfiles_config/fish" "$config/fish"
+ln -sfnv "$dotfiles_config/karabiner" "$config/karabiner"
+ln -sfnv "$dotfiles_config/nvim" "$config/nvim"
 
 chmod +x $HOME/dotfiles/.config/setup_config.sh
 

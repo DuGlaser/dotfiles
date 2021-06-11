e0649b9e627c4190fc1b55f1c466aa4ec918b46f
DuGlaser <duglaser0618@gmail.com>
2021-06-11 23:17:58 +0900

    :wrench: Update the bufferline setting.

--- .config/nvim/lua/plugins/bufferline/setting.lua
+++ .config/nvim/lua/plugins/bufferline/setting.lua
@@ -1,8 +1,27 @@
-require'bufferline'.setup{
+require('bufferline').setup {
   options = {
+    numbers = "none",
+    number_style = "superscript",
     mappings = true,
-    separator_style = "slant",
+    close_command = "bdelete! %d",       
+    right_mouse_command = "bdelete! %d",
+    left_mouse_command = "buffer %d",
+    middle_mouse_command = nil,        
+    max_name_length = 18,
+    max_prefix_length = 15,
     tab_size = 20,
-    show_buffer_close_icons = false
-  };
+    diagnostics = false,
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      return "("..count..")"
+    end,
+    offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "left"}},
+    show_buffer_icons = true,
+    show_buffer_close_icons = false,
+    show_tab_indicators = true,
+    persist_buffer_sort = true,
+    separator_style = "slant",
+    enforce_regular_tabs = true,
+    always_show_bufferline = true,
+    sort_by = 'extension',
+  }
 }

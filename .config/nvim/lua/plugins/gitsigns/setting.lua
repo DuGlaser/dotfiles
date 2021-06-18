e013ecec85b424e17b3a24cd2d5cfbfc5590946d
DuGlaser <duglaser0618@gmail.com>
2021-06-19 00:27:05 +0900

    :wrench: Chagne the gitsigns keymap setting.

--- .config/nvim/lua/plugins/gitsigns/setting.lua
+++ .config/nvim/lua/plugins/gitsigns/setting.lua
@@ -6,4 +6,9 @@ require('gitsigns').setup{
       topdelete    = {hl = 'GitGutterDelete'},
       changedelete = {hl = 'GitGutterChange'},
     },
+    keymaps = {
+      -- Text objects
+      ['o ig'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+      ['x ig'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
+    }
 }

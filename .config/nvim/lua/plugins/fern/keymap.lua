a8b7b5105b5e81b762b547795213214ae1310832
DuGlaser <duglaser0618@gmail.com>
2021-06-27 17:22:15 +0900

    :wrench: Fix the fern keymap setting.

--- .config/nvim/lua/plugins/fern/keymap.lua
+++ .config/nvim/lua/plugins/fern/keymap.lua
@@ -1,3 +1,3 @@
 local remap = vim.api.nvim_set_keymap
 
-remap('n', '<Space>f', '<cmd>Fern . -reveal=% -wait <CR><C-w>=', { noremap = true})
+remap('n', '<Space>f', '<cmd>Fern . -reveal=% <CR><C-w>=', { noremap = true})

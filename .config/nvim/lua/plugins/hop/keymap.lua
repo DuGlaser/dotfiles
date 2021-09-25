95ab23f788fdbb88bb59ed1cb2d9c276418388ee
DuGlaser <duglaser0618@gmail.com>
2021-09-25 19:17:40 +0900

    :wrench: Change the hop keymap setting.

--- .config/nvim/lua/plugins/hop/keymap.lua
+++ .config/nvim/lua/plugins/hop/keymap.lua
@@ -1,4 +1,4 @@
 local remap = vim.api.nvim_set_keymap
 
-remap('','<A-f>', ':HopWord<cr>', { noremap = true, silent = true })
-remap('','<A-d>', ':HopChar2<cr>', { noremap = true, silent = true })
+remap('','<A-f>', '<cmd>HopWord<cr>', { noremap = true, silent = true })
+remap('','<A-d>', '<cmd>HopChar2<cr>', { noremap = true, silent = true })

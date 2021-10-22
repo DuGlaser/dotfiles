ca94150a432690c0d552adbd459fcd4123acaaea
DuGlaser <duglaser0618@gmail.com>
2021-10-23 00:35:11 +0900

    Revert ":wrench: Change the method to load plugin setting."
    This reverts commit ef32c49cd72548448490b8419b9b1c7e1361de89.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,5 +4,14 @@ end
 
 require("plugins.plugins")
 
+require("plugins.bufferline")
+require("plugins.defx")
+require("plugins.gitsign")
+require("plugins.hlslens")
+require("plugins.hop")
+require("plugins.lsp")
+require("plugins.treesitter")
+require("plugins.ultisnips")
+
 runtime("grepper/init.vim")
 runtime("sandwich/init.vim")

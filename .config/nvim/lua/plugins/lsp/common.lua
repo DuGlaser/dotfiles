a6c44beccd14c9ce7649897a12093978c9b48a82
DuGlaser <duglaser0618@gmail.com>
2021-12-24 10:17:22 +0900

    :wrench: Change the lsp setting.

--- .config/nvim/lua/plugins/lsp/common.lua
+++ .config/nvim/lua/plugins/lsp/common.lua
@@ -54,7 +54,7 @@ M.on_attach = function(client, bufnr)
 	-- NOTE: same textDocument/publishDiagnostics
 	vim.diagnostic.config({
 		underline = true,
-		update_in_insert = false,
+		update_in_insert = true,
 		virtual_text = { spacing = 4, prefix = "●" },
 		severity_sort = true,
 	})

d21e319d02b1fba2beef837c6beb4c1169bbe43c
DuGlaser <duglaser0618@gmail.com>
2022-01-18 09:34:30 +0900

    :wrench: Update the `nvim-cmp` setting.

--- .config/nvim/lua/plugins/lsp/nvim-cmp.lua
+++ .config/nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -1,31 +1,31 @@
 local cmp = require("cmp")
 
 local lspkind = {
-	Text = "",
+	Text = "",
 	Method = "",
 	Function = "",
 	Constructor = "",
-	Field = "ﰠ",
-	Variable = "",
+	Field = "",
+	Variable = "",
 	Class = "ﴯ",
 	Interface = "",
 	Module = "",
 	Property = "ﰠ",
-	Unit = "塞",
+	Unit = "",
 	Value = "",
 	Enum = "",
 	Keyword = "",
 	Snippet = "",
 	Color = "",
 	File = "",
-	Reference = "",
+	Reference = "",
 	Folder = "",
 	EnumMember = "",
 	Constant = "",
-	Struct = "פּ",
+	Struct = "",
 	Event = "",
 	Operator = "",
-	TypeParameter = "",
+	TypeParameter = "",
 }
 
 local border = require("plugins.lsp.utils").border
@@ -44,19 +44,15 @@ cmp.setup({
 		end,
 	},
 	formatting = {
-		deprecated = true,
 		format = function(entry, vim_item)
-			vim_item.kind = lspkind[vim_item.kind] .. " " .. vim_item.kind
+			vim_item.kind = string.format("%s %s", lspkind[vim_item.kind], vim_item.kind)
 			vim_item.menu = ({
 				buffer = "[Buffer]",
-				path = "[Path]",
 				nvim_lsp = "[LSP]",
-				luasnip = "[LuaSnip]",
-				user_dictionary = "[Dictionary]",
+				ultisnips = "[Snip]",
+				nvim_lua = "[Lua]",
+				latex_symbols = "[LaTeX]",
 			})[entry.source.name]
-			vim_item.dup = ({
-				buffer = 0,
-			})[entry.source.name] or 1
 			return vim_item
 		end,
 	},
@@ -72,9 +68,12 @@ cmp.setup({
 	},
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp", max_item_count = 20, priority = 51 },
-		{ name = "buffer", max_item_count = 4, priority = 49 },
 		{ name = "ultisnips", max_item_count = 2 },
 		{ name = "path" },
 		{ name = "nvim_lua" },
+	}, {
+		{ name = "buffer", max_item_count = 4, priority = 49 },
 	}),
 })
+
+vim.cmd([[hi CmpItemAbbrMatch guifg=#fe8019]])

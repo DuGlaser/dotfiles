73c1a745d334ed48a0d2764117a10df683d9b25e
DuGlaser <duglaser0618@gmail.com>
2022-02-20 12:13:06 +0900

    :wrench: Del the `atc` alias.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -63,7 +63,6 @@ alias l="exa -a -1"
 alias cls="clear"
 alias rg="rg --colors 'match:bg:yellow' --colors 'match:fg:black' --colors 'match:style:nobold' --colors 'path:fg:green' --colors 'path:style:bold' --colors 'line:fg:yellow' --colors 'line:style:bold'"
 alias grep="rg"
-alias atc="cargo atcoder"
 
 # default EDITOR
 set -gx EDITOR nvim

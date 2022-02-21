1f6e508d018f15b80d429cd4734646e91b977456
DuGlaser <duglaser0618@gmail.com>
2022-02-21 15:54:24 +0900

    :wrench: Update the `vsnip` setting.

--- .config/nvim/lua/plugins/vsnip/init.lua
+++ .config/nvim/lua/plugins/vsnip/init.lua
@@ -3,6 +3,10 @@ imap <expr> <Tab>   vsnip#jumpable(1)  ? '<Plug>(vsnip-jump-next)' : '<Tab>'
 smap <expr> <Tab>   vsnip#jumpable(1)  ? '<Plug>(vsnip-jump-next)' : '<Tab>'
 imap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'
 smap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'
+
+let g:vsnip_filetypes = {}
+let g:vsnip_filetypes.javascriptreact = ['javascript']
+let g:vsnip_filetypes.typescriptreact = ['typescript']
 ]])
 
 vim.g.vsnip_snippet_dir = vim.fn.expand("$HOME/.config/nvim/.vsnip")

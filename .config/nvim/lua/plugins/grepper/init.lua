028c05cf83f71fe0b80d8d49a0fb03d44b2fc1fc
DuGlaser <duglaser0618@gmail.com>
2022-04-21 23:28:27 +0900

    :wrench: Update the nvim-bqf setting.

--- .config/nvim/lua/plugins/grepper/init.lua
+++ .config/nvim/lua/plugins/grepper/init.lua
@@ -1,30 +1,15 @@
+vim.g.grepper = { tools = { "rg", "grep" }, searchreg = 1 }
+vim.cmd(([[
+    aug Grepper
+        au!
+        au User Grepper ++nested %s
+    aug END
+]]):format([[call setqflist([], 'r', {'context': {'bqf': {'pattern_hl': '\%#' . getreg('/')}}})]]))
+
 vim.cmd([[
   nmap gs  <plug>(GrepperOperator)
   xmap gs  <plug>(GrepperOperator)
   nnoremap <Space>g :Grepper -dir repo -tool rg<cr>
-
-  let g:grepper = {
-              \ 'open': 0,
-              \ 'quickfix': 1,
-              \ 'searchreg': 1,
-              \ 'highlight': 0,
-              \ 'tools': ['rg'],
-              \ 'dir': 'repo',
-              \ 'rg': {
-              \   'grepprg': 'rg -H --no-heading --vimgrep --sort-files $* .'
-              \ }}
-
-  augroup vimrcQfClose
-    autocmd!
-    autocmd FileType qf if mapcheck('<esc>', 'n') ==# '' | nnoremap <buffer><silent> <esc> :cclose<bar>lclose<CR> | endif
-  augroup END
-
-  augroup Grepper
-      autocmd!
-      autocmd User Grepper call setqflist([], 'r',
-                  \ {'context': {'bqf': {'pattern_hl': histget('/')}}}) |
-                  \ botright copen
-  augroup END
 
   hi default link BqfPreviewFloat Normal
   hi default link BqfPreviewBorder Normal

b43ce88a8dffd980bea2f39ade6e724f1a6ae03f
DuGlaser <duglaser0618@gmail.com>
2022-05-03 14:28:56 +0900

    :wrench: Update lsp setting.

--- .config/nvim/lua/plugins/lsp/server/eslint.lua
+++ .config/nvim/lua/plugins/lsp/server/eslint.lua
@@ -2,10 +2,11 @@ local common = require("plugins.lsp.common")
 
 return {
 	on_attach = function(client, bufnr)
+		vim.cmd([[autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll]])
 		client.server_capabilities.document_formatting = true
 		common.on_attach(client, bufnr)
 	end,
 	settings = {
-		format = { enable = true },
+		format = true,
 	},
 }
--- .config/nvim/lua/plugins/lsp/server/null-ls.lua
+++ .config/nvim/lua/plugins/lsp/server/null-ls.lua
@@ -1,5 +1,6 @@
 local null_ls = require("null-ls")
 local b = null_ls.builtins
+local utils = require("null-ls.utils").make_conditional_utils()
 
 local M = {}
 
@@ -8,15 +9,14 @@ local common = require("plugins.lsp.common")
 M.prettier_setting_files = { ".prettierrc", ".prettierrc.js", ".prettierrc.json" }
 
 local sources = {
-	b.formatting.prettierd.with({
-		condition = function(utils)
-			return utils.root_has_file(M.prettier_setting_files)
-		end,
-	}),
+	function()
+		return utils.root_has_file(M.prettier_setting_files) and b.formatting.prettierd
+	end,
 	b.formatting.stylua,
 }
 
 M.setup = function()
+	---@diagnostic disable-next-line: redundant-parameter
 	null_ls.setup({
 		sources = sources,
 		on_attach = function(client, bufnr)

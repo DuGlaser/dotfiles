76a7b8ecbe96827ced2f93c42d79f9d7704066d2
DuGlaser <duglaser0618@gmail.com>
2022-08-03 10:24:17 +0900

    :wrench: Update vim keymap setting.

--- .config/nvim/lua/plugins/bufferline/keymap.lua
+++ .config/nvim/lua/plugins/bufferline/keymap.lua
@@ -4,3 +4,4 @@ remap("n", "<C-t><C-t>", "<cmd>BufferLinePick<CR>", { noremap = true, silent = t
 remap("n", "<C-t><C-l>", "<cmd>BufferLineCycleNext<CR>", { noremap = true, silent = true })
 remap("n", "<C-t><C-h>", "<cmd>BufferLineCyclePrev<CR>", { noremap = true, silent = true })
 remap("n", "<C-t><C-p>", "<cmd>Bdelete<CR>", { noremap = true, silent = true })
+remap("n", "<C-t><C-P>", "<cmd>Bdelete!<CR>", { noremap = true, silent = true })

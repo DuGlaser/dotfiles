af0b45d83f3f648354f3a8001d413667f051556b
DuGlaser <duglaser0618@gmail.com>
2022-09-21 17:01:36 +0900

    :wrench: Update nvim setting.

--- .config/nvim/lua/plugins/hop/keymap.lua
+++ .config/nvim/lua/plugins/hop/keymap.lua
@@ -1,4 +1,3 @@
 local remap = vim.api.nvim_set_keymap
 
-remap("", "<A-u>", "<cmd>HopWord<cr>", { noremap = true, silent = true })
-remap("", "<A-i>", "<cmd>HopChar2<cr>", { noremap = true, silent = true })
+remap("", "ss", "<cmd>HopWord<cr>", { noremap = true, silent = true })

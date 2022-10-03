913b1ad1961135c4c27e0fd28a8167947bd609db
DuGlaser <duglaser0618@gmail.com>
2022-10-03 12:37:07 +0900

    :wrench: Update lsp setting.

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -27,7 +27,7 @@ local default_opts = {
 	},
 }
 
-local enableFormatSetting = vim.tbl_deep_extend("force", default_opts, {
+local enableFormatOpts = vim.tbl_deep_extend("force", default_opts, {
 	on_attach = function(client, bufnr)
 		client.server_capabilities.documentFormattingProvider = true
 		common.on_attach(client, bufnr)
@@ -35,39 +35,39 @@ local enableFormatSetting = vim.tbl_deep_extend("force", default_opts, {
 })
 
 local lspconfig = require("lspconfig")
-local opts = {
+local servers = {
 	["angularls"] = require("plugins.lsp.server.angularls"),
-	["ccls"] = enableFormatSetting,
+	["ccls"] = enableFormatOpts,
 	["denols"] = require("plugins.lsp.server.denols"),
 	["eslint"] = require("plugins.lsp.server.eslint"),
-	["gopls"] = enableFormatSetting,
+	["gopls"] = enableFormatOpts,
 	["jsonls"] = require("plugins.lsp.server.jsonls"),
 	["sumneko_lua"] = require("plugins.lsp.server.sumneko_lua"),
 	["tsserver"] = require("plugins.lsp.server.tsserver"),
 	["yamlls"] = require("plugins.lsp.server.yamlls"),
+	["cssls"] = default_opts,
+	["html"] = default_opts,
 }
+
+local function getTableKeys(tab)
+	local keyset = {}
+	for k, _ in pairs(tab) do
+		keyset[#keyset + 1] = k
+	end
+
+	return keyset
+end
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		"angularls",
-		"ccls",
-		"eslint",
-		"gopls",
-		"jsonls",
-		"sumneko_lua",
-		"tsserver",
-		"yamlls",
-		"cssls",
-		"html",
-	},
+	ensure_installed = getTableKeys(servers),
 	automatic_installation = true,
 })
 
 local filter = require("plugins.lsp.filter")
 require("mason-lspconfig").setup_handlers({
 	function(server_name)
-		local opt = opts[server_name] and opts[server_name] or default_opts
+		local opt = servers[server_name] and servers[server_name] or default_opts
 		local new_opt = vim.tbl_deep_extend("force", opt, {
 			on_attach = function(client, bufnr)
 				opt.on_attach(client, bufnr)
@@ -80,7 +80,7 @@ require("mason-lspconfig").setup_handlers({
 })
 
 -- manual setup
-lspconfig.ccls.setup(opts["ccls"])
+lspconfig.ccls.setup(servers["ccls"])
 
 require("lsp_signature").setup({
 	floating_window = false,

b599bf6d63322affb54f0779c1bcfa4031f339e0
DuGlaser <duglaser0618@gmail.com>
2023-01-04 18:44:45 +0900

    :wrench: Update nvim setting.

--- .config/nvim/lua/defaults/init.lua
+++ .config/nvim/lua/defaults/init.lua
@@ -1,4 +1,3 @@
 require("defaults.setting")
 require("defaults.keymap")
-require("defaults.theme")
 require("defaults.filetype")
--- .config/nvim/lua/defaults/theme.lua
+++
@@ -1,6 +0,0 @@
--- vim.cmd("syntax enable")
--- 
--- vim.o.termguicolors = true
--- 
--- vim.g.gruvbox_plugin_hi_groups = 1
--- vim.cmd("colorscheme gruvbox8_soft")

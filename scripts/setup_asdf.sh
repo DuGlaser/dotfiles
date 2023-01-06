07361fd3bc8e1f444c276ddaa0c1219e2030992c
DuGlaser <duglaser0618@gmail.com>
2023-01-06 23:48:02 +0900

    :wrench: Update asdf setup script.

--- scripts/setup_asdf.sh
+++ scripts/setup_asdf.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+if [[ -d "$HOME/.asdf" ]]; then
+	rm -rf "$HOME/.asdf"
+fi
+git clone https://github.com/asdf-vm/asdf "$HOME/.asdf" --branch v0.10.2
 
+echo "load asdf..."
 # shellcheck disable=1091
 source "$HOME/.asdf/asdf.sh"
+
+echo "Install nodejs plugin..."
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+echo "Installation of nodejs plugin is completed!"

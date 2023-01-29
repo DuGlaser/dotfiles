28b626091983283acf20c330c321db474e0baff9
DuGlaser <duglaser0618@gmail.com>
2023-01-29 22:07:27 +0900

    :wrench: WSLに対応

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,8 +2,12 @@
 # for tig
 
 if  [ "$(uname)" == 'Linux' ]; then
-  if type xclip > /dev/null 2>&1; then
-    xclip -selection c
+  if [ "$IS_WSL" == 'true' ]; then
+    win32yank.exe -i
+  else
+    if type xclip > /dev/null 2>&1; then
+      xclip -selection c
+    fi
   fi
 else
  pbcopy

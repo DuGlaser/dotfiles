80f66f3de037c91df3467f4cbacae72ebd2315c3
DuGlaser <duglaser0618@gmail.com>
2023-02-26 22:29:27 +0900

    :wrench: Update.

--- .config/nvim/lua/plugins/gitsign.lua
+++ .config/nvim/lua/plugins/gitsign.lua
@@ -6,6 +6,13 @@ local M = {
 	},
 	config = function()
 		require("gitsigns").setup({
+			current_line_blame = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol",
+				delay = 1000,
+				ignore_whitespace = false,
+			},
 			signs = {
 				add = { hl = "GitGutterAdd", text = "▌" },
 				change = { hl = "GitGutterChange", text = "▌" },

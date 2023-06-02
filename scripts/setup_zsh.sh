8a885d0849deb3546cc123ec9a6c7804f03c8c00
DuGlaser <duglaser0618@gmail.com>
2023-06-02 10:39:04 +0900

    :wrench: Update.

--- scripts/setup_zsh.sh
+++ scripts/setup_zsh.sh
@@ -5,4 +5,5 @@ git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.zsh/zsh-autosuggestions"
 git clone https://github.com/agkozak/zsh-z.git "$HOME/.zsh/zsh-z"
 
+find $HOME/.zsh -name '*?.zsh' | xargs -I@ zsh -c "zcompile @"
 zcompile "$HOME/.zshrc"

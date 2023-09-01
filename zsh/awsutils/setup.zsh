8824d1e12ffb56453ef8a7b4cc90980f5b16a429
DuGlaser <duglaser0618@gmail.com>
2023-09-01 22:20:16 +0900

    update

--- zsh/awsutils/setup.zsh
+++ zsh/awsutils/setup.zsh
@@ -1,7 +1,12 @@
 readonly local awsutils_script_dir=$(dirname "$0")
 
 function awsutils() {
-  target_file="$awsutils_script_dir/src/main.ts"
+  local target_file="$awsutils_script_dir/src/main.ts"
+  local node_modules="$thanos_script_dir/node_modules"
+  if [ ! -d "$node_modules" ]; then
+    (cd $awsutils_script_dir && npm install)
+  fi
+
   $target_file $@
 }
 

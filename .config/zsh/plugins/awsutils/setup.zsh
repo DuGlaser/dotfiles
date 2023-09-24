49e384615ad8ed84fb894b7950a59ecf5db887b7
DuGlaser <duglaser0618@gmail.com>
2023-09-24 22:32:12 +0900

    update

--- .config/zsh/plugins/awsutils/setup.zsh
+++ .config/zsh/plugins/awsutils/setup.zsh
@@ -3,9 +3,9 @@ function awsutils() {
   local target_file="$script_dir/src/main.ts"
   local node_modules="$script_dir/node_modules"
   if [ ! -d "$node_modules" ]; then
-		echo "Installing dependencies..."
+    echo "Installing dependencies..."
     (cd $script_dir && bun install --production --flozen-lockfile)
-		echo "\n"
+    echo "\n"
   fi
 
   $target_file $@

614388a86eaafe08c531bca77567d2e271fc2084
DuGlaser <duglaser0618@gmail.com>
2023-11-04 12:02:10 +0900

    update

--- scripts/setup_ansible.sh
+++ scripts/setup_ansible.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if  [ "$(uname)" == 'Linux' ]; then
-  sudo apt install ansible -y
+  sudo apt update
+  sudo apt install -y software-properties-common
+  sudo apt-add-repository --yes --update ppa:ansible/ansible
+  sudo apt install -y ansible
 else
   xcode-select --install
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"

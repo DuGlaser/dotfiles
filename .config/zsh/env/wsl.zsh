d8d093d70050831deb66412739fefab2c3bde2c7
DuGlaser <duglaser0618@gmail.com>
2023-11-18 12:55:05 +0900

    update

--- .config/zsh/env/wsl.zsh
+++ .config/zsh/env/wsl.zsh
@@ -1,3 +1,12 @@
 export BROWSER="powershell.exe /c start"
 export IS_WSL=true
 alias open="powershell.exe /c start"
+
+function docker() {
+  if [ ! "$(pgrep -x dockerd)" ]; then
+    echo "Starting docker daemon..."
+    sudo service docker start
+  fi
+
+  command docker "$@"
+}
